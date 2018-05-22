05455378c673085f30b18c35193655b3cf8dc7d1
Li, Leon <qingliang.li@nih.gov>
2018-05-22 17:51:00 -0400

    add click support

--- pycli/cli.py
+++ pycli/cli.py
@@ -1,2 +1,22 @@
+import click
+import click_log
+import pkg_resources
+#path = pkg_resources.resource_filename('package_folder', 'lib/java.jar')
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+click_log.basic_config(logger)
+
+@click.group()
+@click_log.simple_verbosity_option(logger)
+@click.option('--verbose', default=False, is_flag=True)
+def cli(verbose):
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.ERROR)
+
+@cli.command("name_of_the_command")
 def main():
     pass
+
